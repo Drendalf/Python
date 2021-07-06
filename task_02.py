0082e26a6661c901170aadbd44444fc03e559fd7
Drendalf <82385861+Drendalf@users.noreply.github.com>
2021-07-06 17:05:20 +0300

    DZ_04

---
+++ task_02.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def currency_rates(currency, currancies):
+    return round(currancies.get(currency.lower()), 2)
+
+
+currancies = {}
+r = requests.get("http://www.cbr.ru/scripts/XML_daily.asp")
+if r.status_code == 200:
+    for info_text in r.text.split("<Nominal>")[1:]:
+        info = info_text.split("</Value>")[0].split("</Nominal><Name>")
+        nominal = int(info[0])
+        info2 = info[1].split("</Name><Value>")
+        currency_type = info2[0]
+        value = float(info2[1].replace(",", "."))
+        currancies[currency_type.lower()] = value / nominal
+
+print(currency_rates("Евро", currancies))
